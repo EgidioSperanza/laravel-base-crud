dee05aa06274ac2c896623f598fab65f607c543e
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-03-10 17:34:28 +0100

    Custom request validation error messages

--- app/Http/Requests/ComicStoreRequest.php
+++ app/Http/Requests/ComicStoreRequest.php
@@ -33,4 +33,22 @@ class ComicStoreRequest extends FormRequest
             'type' => 'required|min:5',
         ];
     }
+    public function messages(){
+        return[
+            'title.required' => 'Il titolo è obbligatorio',  
+            'title.min' => 'Il titolo deve essere di alemeno 3 caratteri',  
+            'description.required' => 'La descrizione è obbligatoria',  
+            'description.min' => 'La descrizione deve essere di alemeno 50 caratteri',  
+            'thumb.url' => 'L\'immagine della copertina deve essere fornita tramite un indirizzo(url)',
+            'price.required' => 'Il prezzo è obbligatorio',  
+            'price.min' => 'Il prezzo deve essere fornito in formato  numerico',  
+            'series.required' => 'Deve essere indicata la serie di appartenenza del fumetto',  
+            'series.min' => 'La serie di appartenenza del fumetto deve avere un nome di almeno 5 caratteri',  
+            'sale_date.required' => 'La data di inizio vendita è obbligatoria',  
+            'sale_date.date_format' => 'La data di inizio vendita deve essere formattata come: AAAA-mese-giorno',  
+            'type.required' => 'Deve essere indicato il tipo di fumetto (solitamente comics bboks o graphic novel)',  
+            'type.min' => 'La specifica del tipo deve essere di almeno 5 caratteri',  
+
+        ];
+    }
 }

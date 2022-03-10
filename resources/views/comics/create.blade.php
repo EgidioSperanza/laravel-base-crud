fa29d29ebe6a557602710d7db0ec8de416686f6d
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-03-10 17:03:29 +0100

    create/store validation and error messages apply

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -11,31 +11,52 @@
     @csrf
     <div class="mb-3">
         <label for="title" class="form-label">Titolo</label>
-        <input type="text" class="form-control" name="title" id="title" aria-describedby="helpId" placeholder="Title">
+        <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title" aria-describedby="helpId" placeholder="Title" value="{{old('title')}}">
+        @error('title')
+        <div class="invalid-feedback bg-warning p-2">{{$message}}</div>
+        @enderror
     </div>
     <div class="mb-3">
         <label for="thumb" class="form-label">Copertina</label>
-        <input type="text" class="form-control" name="thumb" id="thumb" aria-describedby="helpId" placeholder="Cover link">
+        <input type="text" class="form-control @error('thumb') is-invalid @enderror" name="thumb" id="thumb" aria-describedby="helpId" placeholder="Cover link" value="{{old('thumb')}}">
+        @error('thumb')
+        <div class="invalid-feedback bg-warning p-2">{{$message}}</div>
+        @enderror
     </div>
     <div class="mb-3">
         <label for="price" class="form-label">Prezzo</label>
-        <input type="text" class="form-control" name="price" id="price" aria-describedby="helpId" placeholder="Price">
+        <input type="text" class="form-control @error('price') is-invalid @enderror" name="price" id="price" aria-describedby="helpId" placeholder="Price" value="{{old('price')}}">
+        @error('price')
+        <div class="invalid-feedback bg-warning p-2">{{$message}}</div>
+        @enderror
     </div>
     <div class="mb-3">
         <label for="series" class="form-label">Serie</label>
-        <input type="text" class="form-control" name="series" id="series" aria-describedby="helpId" placeholder="Series">
+        <input type="text" class="form-control @error('series') is-invalid @enderror" name="series" id="series" aria-describedby="helpId" placeholder="Series" value="{{old('series')}}">
+        @error('series')
+        <div class="invalid-feedback bg-warning p-2">{{$message}}</div>
+        @enderror
     </div>
     <div class="mb-3">
         <label for="sale_date" class="form-label">In vendita dal</label>
-        <input type="text" class="form-control" name="sale_date" id="sale_date" aria-describedby="helpId" placeholder="Sale date start">
+        <input type="text" class="form-control @error('sale_date') is-invalid @enderror" name="sale_date" id="sale_date" aria-describedby="helpId" placeholder="Sale date start" value="{{old('sale_date')}}">
+        @error('sale_date')
+        <div class="invalid-feedback bg-warning p-2">{{$message}}</div>
+        @enderror
     </div>
     <div class="mb-3">
         <label for="type" class="form-label">Tipo di Comics</label>
-        <input type="text" class="form-control" name="type" id="type" aria-describedby="helpId" placeholder="Comic Book | Graphic Novel">
+        <input type="text" class="form-control @error('type') is-invalid @enderror" name="type" id="type" aria-describedby="helpId" placeholder="Comic Book | Graphic Novel" value="{{old('type')}}">
+        @error('type')
+        <div class="invalid-feedback bg-warning p-2">{{$message}}</div>
+        @enderror
     </div>
     <div class="mb-3">
         <label for="description" class="form-label">Descrizione</label>
-        <textarea class="form-control" name="description" id="description" aria-describedby="helpId" placeholder="Description"></textarea>
+        <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" aria-describedby="helpId" placeholder="Description">{{old('description')}}</textarea>
+        @error('description')
+        <div class="invalid-feedback bg-warning p-2">{{$message}}</div>
+        @enderror
     </div>
     <div class="d-flex justify-content-end">
         @include('partials.return', ['routeTarget'=>'comics.index'])
